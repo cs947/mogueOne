eb654ce978040b6fc7bc41a30e4b66e083dfd607
Fujia Ren <fr233@cornell.edu>
2019-06-15 11:07:14 -0400

    Adding base swift files

---
+++ AffixApp/Affix/Affix/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Affix
+//
+//  Created by Fujia Ren on 6/15/19.
+//  Copyright © 2019 Fujia Ren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var name: UILabel!
+    var id: UILabel!
+    var rank: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
